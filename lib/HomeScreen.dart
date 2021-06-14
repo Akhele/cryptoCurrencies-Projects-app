4800b9a49039fd75f9f486f6c421c0d5f5c9b1fc
Akhele <Moneimb8@gmail.com>
2021-06-14 13:25:31 +0100

    animate the two screens by clicking the Menu button

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -8,23 +8,52 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // Animation Variables :
+  double xoffset = 0, yoffset = 0, scalefactor = 1;
+
+  bool isDrawerOpen = false;
+
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
+      transform: Matrix4.translationValues(xoffset, yoffset, 0)
+        ..scale(scalefactor),
       duration: Duration(milliseconds: 250),
       color: Colors.white,
       child: Column(
         children: [
-          SizedBox(height: 50,),
+          SizedBox(
+            height: 50,
+          ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {},
-                ),
+                isDrawerOpen
+                    ? IconButton(
+                        onPressed: () {
+                          setState(() {
+                            xoffset = 0;
+                            yoffset = 0;
+                            scalefactor = 1;
+
+                            isDrawerOpen = false;
+                          });
+                        },
+                        icon: Icon(Icons.undo))
+                    : IconButton(
+                        icon: Icon(Icons.menu),
+                        onPressed: () {
+                          setState(() {
+                            xoffset = 230;
+                            yoffset = 250;
+                            scalefactor = 0.6;
+
+                            isDrawerOpen = true;
+                          });
+                        },
+                      ),
                 Column(
                   children: [
                     Text("Crypto Currencies"),
@@ -42,7 +71,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 CircleAvatar(),
               ],
-
             ),
           )
         ],
